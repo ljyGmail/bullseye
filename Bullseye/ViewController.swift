223180f6b58b24a6b91ca1b25132ae40fdf72741
liangjinyong <liangjy881@gmail.com>
2024-08-29 21:31:06 +0900

    slider and labels

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -8,19 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
-        
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
+  
+  var currentValue: Int = 50
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func showAlert() {
+    let message = "The value of the slider is: \(currentValue)"
+    
+    let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
+    
+    let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+    
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+  }
+  
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    currentValue = lroundf(slider.value)
+  }
 }
 
